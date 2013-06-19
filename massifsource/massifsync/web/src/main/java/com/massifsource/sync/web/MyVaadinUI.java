4bff064204dc9f2e28cd05b4f7fd515c285a2f39
Nikolay Sarychev <abeliandevelopment@gmail.com>
2013-06-18 19:26:14 -0600

    Blah

--- massifsource/massifsync/web/src/main/java/com/massifsource/sync/web/MyVaadinUI.java
+++ massifsource/massifsync/web/src/main/java/com/massifsource/sync/web/MyVaadinUI.java
@@ -1,7 +1,9 @@
 package com.massifsource.sync.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.massifsource.sync.db.h2.repository.DirectoryRepository;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -15,22 +17,27 @@ import com.vaadin.ui.VerticalLayout;
 @Component
 public class MyVaadinUI extends UI
 {
-
+	
+	@Autowired
+	DirectoryRepository directoryRepository;
+	
 	private static final long serialVersionUID = 2582897831380309074L;
 
 	@Override
     protected void init(VaadinRequest request) {
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
-        setContent(layout);
+        setContent(new TestComposite());
         
         Button button = new Button("Click Me");
         button.addClickListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
                 layout.addComponent(new Label("Thank you for clicking"));
+                
             }
         });
         layout.addComponent(button);
+        layout.addComponent(new TestComposite());
     }
 
 }
